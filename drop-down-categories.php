f408cd162d0e422a6769f8fe152ab6081b17c029
Hanna Holm <hanna.holm@hotmail.com>
2024-05-28 11:56:18 +0200

    Fixed categories links in header

--- drop-down-categories.php
+++ drop-down-categories.php
@@ -1,24 +1,28 @@
 <?php
+
+
+
 echo '<li class="menu-item">Kategorier
 <ul id="categories-drop-down-menu">
       <li class="drop-down-item">
-            <a href="/adfsgd">Bord</a>
+      <a href="categories/showcategory.php?category_id=1">Sittmöbler</a>
       </li>
       <li class="drop-down-item">
-            <a href="/adfsgd">Sängar</a>
+      <a href="categories/showcategory.php?category_id=2">Bord</a>
       </li>
       <li class="drop-down-item">
-            <a href="/adfsgd">Förvaring</a>
+      <a href="categories/showcategory.php?category_id=3">Sängar</a>
       </li>
       <li class="drop-down-item">
-            <a href="/adfsgd">Sittmöbler</a>
+      <a href="categories/showcategory.php?category_id=4">Förvaring</a>
       </li>
       <li class="drop-down-item">
-            <a href="/adfsgd">Utemöbler</a>
+      <a href="categories/showcategory.php?category_id=5">Utemöbler</a>
       </li>
       <li class="drop-down-item">
-            <a href="/adfsgd">Övrigt</a>
+      <a href="categories/showcategory.php?category_id=6">Övrigt</a>
       </li>
 </ul>
 </li>';
+
 ?>
